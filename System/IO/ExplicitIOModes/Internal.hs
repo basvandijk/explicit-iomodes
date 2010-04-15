293b28afa8d862f3c3c1efb9064ce34eb609dab6
Bas van Dijk <v.dijk.bas@gmail.com>
2010-04-15 11:31:30 +0200

    Removed SIO synonym for System.IO
    Ignore-this: 328167be431fc965968377a7eced43ec
    
    darcs-hash:20100415093130-ae560-a7fb2c52b0a145ce65dceca649a8ee263064c27a.gz

--- System/IO/ExplicitIOModes/Internal.hs
+++ System/IO/ExplicitIOModes/Internal.hs
@@ -3,22 +3,22 @@
 module System.IO.ExplicitIOModes.Internal where
 
 -- from base:
-import Text.Show                  ( Show )
-import Data.Eq                    ( Eq )
-import Data.Typeable              ( Typeable )
-import qualified System.IO as SIO ( Handle )
+import Text.Show           ( Show )
+import Data.Eq             ( Eq )
+import Data.Typeable       ( Typeable )
+import qualified System.IO ( Handle )
 
 -- | A handle to a file with an explicit IOMode.
 --
 -- Wraps: @System.IO.@'SIO.Handle'.
 newtype Handle ioMode = Handle
-    { -- | Retrieves the regular @System.IO.@'SIO.Handle'.
-      regularHandle ∷ SIO.Handle
+    { -- | Retrieves the regular @System.IO.@'System.IO.Handle'.
+      regularHandle ∷ System.IO.Handle
     }
     deriving ( Show, Eq, Typeable )
 
-wrap ∷ (SIO.Handle → α) → (Handle ioMode → α)
+wrap ∷ (System.IO.Handle → α) → (Handle ioMode → α)
 wrap f = \h → f (regularHandle h)
 
-wrap2 ∷ (SIO.Handle → β → α) → (Handle ioMode → β → α)
+wrap2 ∷ (System.IO.Handle → β → α) → (Handle ioMode → β → α)
 wrap2 f = \h y → f (regularHandle h) y
