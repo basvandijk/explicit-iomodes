78dfeb7038b85e5f35af3bbd25b439a254c29ef3
Bas van Dijk <v.dijk.bas@gmail.com>
2010-08-11 07:48:26 +0200

    Actually added /System/IO/ExplicitIOModes/Unsafe.hs
    Ignore-this: 79a3e5bb51a71d81ec0d831cb3939288
    
    darcs-hash:20100811054826-ae560-50cc2de41049bc69c0b2fe0895b60ecb4524bc03.gz

---
+++ System/IO/ExplicitIOModes/Unsafe.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE NoImplicitPrelude, UnicodeSyntax #-}
+
+module System.IO.ExplicitIOModes.Unsafe ( regularHandle
+                                        , wrap, wrap2
+                                        ) where
+
+-- from base:
+import qualified System.IO ( Handle )
+
+-- from ourselves:
+import System.IO.ExplicitIOModes.Internal ( Handle, regularHandle )
+
+wrap ∷ (System.IO.Handle → α) → (Handle ioMode → α)
+wrap f = \h → f (regularHandle h)
+
+wrap2 ∷ (System.IO.Handle → β → α) → (Handle ioMode → β → α)
+wrap2 f = \h y → f (regularHandle h) y
